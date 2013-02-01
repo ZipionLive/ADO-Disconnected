d9ebfd2574c748926c569d90b40fab24801b0601
Alexandre Engelhardt <alexandre@zipion-live.be>
2013-02-01 13:40:17 +0100

    Début de projet
    Projet d'étude sur ADO et les classes déconnectées. Une table est créée
    avec des colonnes et une ligne y est ajoutée.

---
+++ ADO-Disconnected/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data;
+using System.Text;
+
+namespace ADO_Disconnected
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataTable cave = new DataTable("Cave"); // création de la table "Cave" (vide pour l'instant)
+
+            DataColumn id = new DataColumn("ID");
+            id.DataType = typeof(int);
+            id.AutoIncrement = true; // active l'autoincrémentation de la colonne
+            id.AutoIncrementSeed = 1; // valeur de départ pour l'autoincrémentation
+            id.AutoIncrementStep = 1; // pas pour l'autoincrémentation
+            cave.Columns.Add(id);
+            cave.PrimaryKey = new DataColumn[] { id }; // désignation de la colonne "ID" comme clé primaire de la table
+
+            DataColumn vin = new DataColumn("Vin"); // création d'une colonne "Vin"
+            vin.DataType = typeof(string); // le type de données est string par défaut, cette ligne est donc optionelle
+            vin.Unique = true; // détermine si les valeurs de la colonnes doivent être uniques (false par défaut)
+            vin.AllowDBNull = false; // détermine si la colonne accepte les valeurs NULL (true par défaut)
+            vin.Caption = "Vin"; // nom que portera la colonne dans la représentation graphique (par défaut, c'est le nom de la colonne spécifié lors de la déclaration)
+            cave.Columns.Add(vin); // la colonne "Vin" est ajoutée à la table "Cave"
+
+            DataColumn annee = new DataColumn("Annee", typeof(int)); // on peut utiliser le constructeur à 2 paramètres pour déclarer une nouvelle colonne tout en spécifiant son type
+            annee.AllowDBNull = false;
+            annee.Caption = "Année";
+            cave.Columns.Add(annee);
+
+            DataColumn marque = new DataColumn("Marque");
+            marque.MaxLength = 35;  // détermine la taille maximale dans le cas d'un string (-1 par défaut, càd illimité)
+            marque.AllowDBNull = false;
+            cave.Columns.Add(marque);
+
+            // la colonne suivante est une colonne dérivée des colonnes "Marque" et "Année"
+            DataColumn marqueEtAnnee = new DataColumn("MarqueEtAnnee");
+            marqueEtAnnee.MaxLength = 40;
+            marqueEtAnnee.Expression = "Annee + ' ' + Marque"; // la propriété "Expression" permet de concaténer les valeurs de plusieurs colonnes
+            marqueEtAnnee.Caption = "Marque et Année";
+            cave.Columns.Add(marqueEtAnnee);
+
+            // remplissage de la table
+            DataRow newCave = cave.NewRow(); // création de la ligne à insérer
+            newCave["Vin"] = "Beaujolais";
+            newCave["Marque"] = "Grand Cru";
+            newCave["Annee"] = 1982;
+            cave.Rows.Add(newCave); // ajout de la ligne à la table "Cave"
+        }
+    }
+}
